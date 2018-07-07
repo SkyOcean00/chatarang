c97f273a22f3afee60c7192b3c9c3df73aa5db24
Cheng Qian <qianch@indiana.edu>
2018-07-07 10:52:40 -0500

    Styling Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react'
 import Userinfo from'./Userinfo'
-
+import RoomList from './RoomList'
 
 const styles = {
+    sidebar: {
+        backgroundColor: '#333344',
+        color: 'rgba(255, 255, 255, 0.8)',
+        width: '12rem',
+        padding: '1rem 0',
+        display: 'flex',
+        flexDirection: 'column',
+      },
+    
+      h1: {
+        color: 'white',
+        fontSize: '1.2rem',
+        marginTop: 0,
+        padding: '0 1rem',
+      },
     sidebar: {
         backgroundColor: '#944743',
         color: 'rgba(255, 255, 255, 0.8)',
@@ -41,13 +56,8 @@ class Sidebar extends Component {
 
                 <h1 style={styles.h1}>XTBC 18</h1>
 
-                <nav className="RoomList" style={styles.children}>
-                    <h2>Rooms</h2>
-                    <ul style={ styles.coco}>
-                        <li><a href="#">general</a></li>
-                        <li><a href="#">random</a></li>
-                    </ul>
-                </nav>
+                <RoomList />
+                
             </aside>
         );
     }
